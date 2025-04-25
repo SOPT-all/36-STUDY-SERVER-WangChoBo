72f6f4ff6c31b83159fd155bcbe97fa1e7e20f65
dajinYang <didekwls0104@naver.com>
2025-04-25 19:01:13 +0900

    initial settings

---
+++ src/main/java/sopt/wangchobo/WangchoboApplication.java
@@ -0,0 +1,13 @@
+package sopt.wangchobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangchoboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WangchoboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopt/wangchobo/WangchoboApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.wangchobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WangchoboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
